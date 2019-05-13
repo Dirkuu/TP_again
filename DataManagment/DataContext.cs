f0dbdae32f81c6a3bd7bb470e4a1e121ad0c21ff
211954 Jeżowski Wojciech <211954@edu.p.lodz.pl>
2019-05-13 22:13:41 +0200

    DataManagment project

---
+++ DataManagment/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Model;
+
+namespace DataManagment
+{
+    class DataContext
+    {
+        private List<Worker> workerList;
+        private List<Machine> machineList;
+
+        public DataContext()
+        {
+            WorkerList = new List<Worker>();
+            MachineList = new List<Machine>();
+        }
+
+        public List<Worker> WorkerList { get => workerList; set => workerList = value; }
+        public List<Machine> MachineList { get => machineList; set => machineList = value; }
+    }
+}
