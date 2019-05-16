bdc10eba48f099f6f678339351579b0c23cedeba
Wojciech Jeżowski <dirkuu@gmail.com>
2019-05-16 21:51:03 +0200

    DataContext collectionChanged()

--- DataManagment/DataContext.cs
+++ DataManagment/DataContext.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using Model;
 
 namespace DataManagment
@@ -14,6 +13,30 @@ namespace DataManagment
         {
             WorkerList = new List<Worker>();
             MachineList = new List<Machine>();
+        }
+
+        private void collectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            Console.WriteLine("Action for this event: {0}", e.Action);
+
+            if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+            {
+                Console.WriteLine("Here are the new items:");
+                foreach (var p in e.NewItems)
+                {
+                    Console.WriteLine(p.ToString());
+                }
+            }
+
+            if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
+            {
+                Console.WriteLine("Here are the old items:");
+                foreach (var p in e.OldItems)
+                {
+                    Console.WriteLine(p.ToString());
+                }
+                Console.WriteLine();
+            }
         }
 
         public List<Worker> WorkerList { get => workerList; set => workerList = value; }
